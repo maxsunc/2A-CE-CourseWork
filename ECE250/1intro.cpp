e7ec5777583577561d583572e516af1853dce2e2
Max Sun <130513985+maxsunc@users.noreply.github.com>
2025-09-18 17:28:32 -0400

    updates

--- ECE250/1intro.cpp
+++ ECE250/1intro.cpp
@@ -12,3 +12,5 @@ int main()
 // if n=1, done O(1)
 //  recursively sort A[1..ceil(n/2)] and A[ceil(n/2)]+1..n. O
 //  3. merging the 2 sorted array O(n) just use two pointer
+
+// T(n) = 2T(n/2) + O(n) => O(n log n)
