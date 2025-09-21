0c1d5421087914e869d0d9a4220535ffbaeb7b4d
Max Sun <130513985+maxsunc@users.noreply.github.com>
2025-09-20 20:09:28 -0400

    updated notes

--- ECE250/1intro.cpp
+++ ECE250/1intro.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
-int main()
+class Vector2D
 {
-    std::cout << " hello world" << std::endl;
-    return 0;
-}
+public:
+    double x;
+    double y;
+
+    // Default constructor (required for arrays)
+    Vector2D() : x(0.0), y(0.0) {}
+
+    Vector2D(double x_val, double y_val) : x(x_val), y(y_val) {}
+};
 
 // T(3n + 6) => O(n)
 
@@ -14,3 +20,21 @@ int main()
 //  3. merging the 2 sorted array O(n) just use two pointer
 
 // T(n) = 2T(n/2) + O(n) => O(n log n)
+
+int main()
+{
+    Vector2D obj1 = Vector2D(0.0, 0.0);        // Direct construction
+    Vector2D obj2 = *(new Vector2D(0.0, 0.0)); // new + dereference (new keyword gives pointer to object always)
+    // obj2[i] means obj2[0] points to first element, obj2[1] points to second element (4 bytes apart since double is 4 bytes)
+    // strings
+    std::string example; //
+
+    // getting input like this compute 1 4
+    std::cin >> example; // read until space
+    std::cout << example << std::endl;
+    // read next guy
+    std::cin >> example; // reads until space again
+    std::cout << example << std::endl;
+    std::cin >> example; // reads until space again
+    std::cout << example << std::endl;
+}
