6687e091283eaf388dc16f496169e71251502508
Max Sun <130513985+maxsunc@users.noreply.github.com>
2025-09-22 13:51:25 -0400

    updated assembly syntax

---
+++ ECE222/syntax.asm
@@ -0,0 +1,27 @@
+
+/* This is a comment in assembly language */
+
+/* Variables are stored in the .data section */
+.data
+addi x5, x0, 10   # x5 = 10
+addi x6, x0, 20   # x6 = 20
+li x7, 30         # x7 = 30 (load immediate)
+
+/* RISC-V data transfer
+    lw = load word (4 bytes) brings data from memory to register
+    sw = store word (4 bytes) writes data from register to memory
+    lb = load byte (1 byte) brings 1 byte from memory to register
+    sb = store byte (1 byte) writes 1 byte from register to memory
+    */
+
+    /* load word / store word example */
+    /* c code: int A[100]; x8 = A[0]; */
+       int x = A[1]; x = x5
+       x++;
+       A[1] = x;   
+       
+    */
+    lw x1 4(x8)  # load A[1] into x1 (4 bytes offset)
+    addi x1, x1, 1 # increment x1
+    sw x1, 4(x8)  # store x1 back into A[1]
+    
